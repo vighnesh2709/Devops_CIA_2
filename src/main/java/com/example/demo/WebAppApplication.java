0d91ca60e473dc2da8f7dd12acb88bef7b6ae781
vighnesh2709 <bvighnesh27@gmail.com>
2025-11-19 14:57:13 +0530

    new check

--- src/main/java/com/example/demo/WebAppApplication.java
+++ src/main/java/com/example/demo/WebAppApplication.java
@@ -20,7 +20,7 @@ public class WebAppApplication {
 
     @RequestMapping("/test")
     public String testing(){
-        return ("Priliminary checks 4 !");
+        return ("Priliminary checks 5 !");
     }
 
 }
